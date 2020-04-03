b2e447f1f03c4018f6922e24c0f30c1038e27d7d
Christian Rebollar <christian.rebollar@gmail.com>
2020-04-03 13:31:02 -0400

    Agregando scripts para CI/CD

--- scripts/builddocker.sh
+++ scripts/builddocker.sh
@@ -1,1 +1,2 @@
-docker build -t gcr.io/plataforma-mexico-covid19/gcloudjnlp:1.0.1 .
+docker build -t gcr.io/plataforma-mexico-covid19/gcloudjnlp:1.0.5 . && \
+docker push gcr.io/plataforma-mexico-covid19/gcloudjnlp:1.0.5

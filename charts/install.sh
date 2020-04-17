b6aead203ef3f3012b2af61c6907496d3604a613
Christian Rebollar <christian.rebollar@gmail.com>
2020-04-16 23:50:06 -0400

    Adding test values files

--- charts/install.sh
+++ charts/install.sh
@@ -4,6 +4,13 @@ echo "2do parametro = namespace"
 echo "3er parametro = ruta al archivo kubeconfig"
 echo "4to parametro = install|upgrade"
 echo "5to paremetro = version a instalar "
+echo "6to paremetro = archivo de values "
 echo "$4 this helm chart $1..."
-helm $4 $1-helm --namespace=$2 --kubeconfig=$3 ./$1 --set image.version=$5 --set-string timestamp=$(date +%s) && \
-echo $5 > ./$1/version
+
+if [[ $# -eq 6 ]]; then
+	echo "Doing push $#"
+	helm $4 $1-helm -f ./$1/$6 --namespace=$2 --kubeconfig=$3 ./$1 --set image.version=$5 --set-string timestamp=$(date +%s)  	
+else 
+	echo "Docker push $# "
+	helm $4 $1-helm -f ./$1/values.yaml --namespace=$2 --kubeconfig=$3 ./$1 --set image.version=$5 --set-string timestamp=$(date +%s)
+fi
